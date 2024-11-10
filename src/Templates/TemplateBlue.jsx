a26f6a0916578ee9d9fb74f6b1a22018243c4649
Deepak Gautam <38098415+styloxstar@users.noreply.github.com>
2024-11-10 22:40:04 +0530

    little change to light theme

--- src/Templates/TemplateBlue.jsx
+++ src/Templates/TemplateBlue.jsx
@@ -7,10 +7,11 @@ const TemplateBlue = () => {
   return (
     <>
         <section id="templateBlue" className='dark:bg-[#0C1641] bg-[#E1E1E1] flex'>
-            <div id="sidebar" className="basis-2/12 dark:bg-[#344272] dark:text-white bg-[#02DAFF] m-3 rounded-md h-[calc(100vh-1.5rem)] dark:bg-opacity-80">
+            <div id="sidebar" className="basis-2/12 dark:bg-[#344272] dark:text-white bg-[#4265D0] m-3 rounded-md h-[calc(100vh-1.5rem)] dark:bg-opacity-80">
                 <Sidebar />
             </div>
-            <div id="mainBody" className='basis-10/12 dark:text-white dark:bg-[#344272] bg-[#FFFFFF] m-3 rounded-md h-[calc(100vh-1.5rem)] dark:bg-opacity-25' >
+            <div id="mainBody" className='basis-10/12 dark:text-white  my-3 me-3 rounded-md h-[calc(100vh-1.5rem)] dark:bg-opacity-25' >
+            {/* dark:bg-[#344272] bg-[#FFFFFF] */}
                 <MainBody />
             </div>
         </section>
